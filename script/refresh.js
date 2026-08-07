496f22cc56c5baaf11d3735b5e0974aa24a90b28
jiisloth <joonas.val@gmail.com>
2026-08-07 11:50:40 +0300

    made the thing automatically refresh

--- script/refresh.js
+++ script/refresh.js
@@ -1,8 +1,33 @@
-const refreshIntervalHours = 1
+const fullRefreshIntervalHours = 24
+const hiddenRefreshIntervalMinutes = 1
 
+let current = 1
 
-function reload() {
+function fullReload() {
   window.location.reload();
 }
+function getNextId() {
+  let next = current + 1
+  if (next === 3) next = 1
+  return "refresh" + next
+}
+function hiddenReload() {
+  let next = getNextId()
+  document.getElementById(next).classList.add('behind');
+  document.getElementById(next).classList.add('invisible');
+  document.getElementById(next).src = document.getElementById(next).src
+  setTimeout(hiddenReloadShow, 1000 * 2)
+}
+function hiddenReloadShow() {
+  let next = getNextId()
+  document.getElementById("refresh" + current).classList.remove('front');
+  document.getElementById(next).classList.remove('behind');
+  document.getElementById(next).classList.remove('invisible');
+  document.getElementById(next).classList.add('front');
+  current += 1
+  if (current === 3) current = 1
+  setTimeout(hiddenReload, 1000 * 60 * hiddenRefreshIntervalMinutes)
+}
 
-setTimeout(reload, 1000 * 60 * 60 * refreshIntervalHours)
+setTimeout(fullReload, 1000 * 60 * 60 * fullRefreshIntervalHours)
+setTimeout(hiddenReload, 1000 * 60 * hiddenRefreshIntervalMinutes)
