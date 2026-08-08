7966189c83d60e09e46e9526a18a68d2210cb782
jiisloth <joonas.val@gmail.com>
2026-08-08 18:12:46 +0300

    pertti stuck

--- script/pertti.js
+++ script/pertti.js
@@ -1,5 +1,5 @@
 
-var lastTimePerttiGotStuck = "2026-08-05"
+var lastTimePerttiGotStuck = "2026-08-07"
 
 
 var daysSinceStuck = Math.round((Date.now() - Date.parse(lastTimePerttiGotStuck))/(1000*60*60*24))
