a80af118f979a348674287f0c6f44fd3fd1f948b
Aleksi Seppänen <aleksi@seppanen.dev>
2026-09-11 13:26:43 +0300

    Add missing semicolons in pertti.js

--- script/pertti.js
+++ script/pertti.js
@@ -1,6 +1,6 @@
 
-var lastTimePerttiGotStuck = "2026-08-07"
+var lastTimePerttiGotStuck = "2026-08-07";
 
 
-var daysSinceStuck = Math.round((Date.now() - Date.parse(lastTimePerttiGotStuck))/(1000*60*60*24) + 0.5)
-document.getElementById("perttiStuckValue").innerText = daysSinceStuck
+var daysSinceStuck = Math.round((Date.now() - Date.parse(lastTimePerttiGotStuck))/(1000*60*60*24) + 0.5);
+document.getElementById("perttiStuckValue").innerText = daysSinceStuck;
