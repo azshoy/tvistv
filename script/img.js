64560dc692a4d6b24c98630b26c1e68b598f3f6c
jiisloth <joonas.val@gmail.com>
2026-09-24 12:11:42 +0300

    fix image sizing and make rotate random

--- script/img.js
+++ script/img.js
@@ -1,5 +1,6 @@
 
 async function get_random_image() {
+  document.getElementById("img").style.transform = "rotate(" + (Math.random()*6 - 3).toFixed(1) + "deg)"
   const result = await APIget("/random-image");
   if (result && result.url) {
     document.getElementById("img").innerHTML = "<img src='" + result.url + "' alt=\"xdd\">";
